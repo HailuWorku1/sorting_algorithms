da8ec8b3ad3c4ef4b1b9f6332e7c0e640e9fbc3b
Hailu Worku <hailuworku1@yahoo.com>
2022-07-18 22:54:52 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,68 +1,61 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - Sorts a linked list of integers in ascending order
- * @list: The list to be sorted
- * Return: Nothing
- * Description: This function sorts a linked list of integers in ascending order
+ * len_list - returns the length of a linked list
+ * @h: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
+/**
+ * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * @list: double pointer to the list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev;
-	listint_t *next;
+	listint_t *curr = NULL, *one = NULL;
+	listint_t *two = NULL, *three = NULL, *four = NULL;
 
-	if (!list || !(*list))
+	if (!list || !(*list) || len_list(*list) < 2)
 		return;
 
-	if (!(*list)->next)
-		return;
+	curr = *list;
 
-	current = *list;
-	next = current->next;
-
-	while (next)
+	while (curr)
 	{
-		if (current->n > next->n)
+		if (curr->prev && curr->n < curr->prev->n)
 		{
-			if (current->prev)
-			{
-				current->prev->next = next;
-				next->prev = current->prev;
-				print_list(*list);
-			}
+			one = curr->prev->prev;
+			two = curr->prev;
+			three = curr;
+			four = curr->next;
+
+			two->next = four;
+			if (four)
+				four->prev = two;
+			three->next = two;
+			three->prev = one;
+			if (one)
+				one->next = three;
 			else
-			{
-				*list = next;
-				next->prev = NULL;
-				print_list(*list);
-			}
-			prev = next->prev;
+				*list = three;
+			two->prev = three;
+			curr = *list;
 			print_list(*list);
-			while (prev && prev->n > next->n)
-			{
-				next->prev = prev->prev;
-				prev->next = next->next;
-				if (next->next)
-					next->next->prev = prev;
-				prev->prev = next;
-				next->next = prev;
-				prev = next->prev;
-				print_list(*list);
-			}
-			next->prev = prev;
-			print_list(*list);
-			if (prev)
-				prev->next = next;
-			else
-				*list = next;
-			current = next;
-			next = current->next;
-			print_list(*list);
+			continue;
 		}
 		else
-		{
-			current = next;
-			next = current->next;
-			print_list(*list);
-		}
+			curr = curr->next;
 	}
 }
